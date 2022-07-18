e68c84cb766ee44b65a4e8227615a80c774605db
topcook <codecooker1996@gmail.com>
2022-07-18 15:36:38 -0300

    added phase 3

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -32,7 +32,10 @@ const Roadmap = () => {
                                 Design webpage
                             </div>
                             <div class='mb-2'>
-                                Our developers will design an interactive webpage powered by web3 for our community. Our first version will be a regular interface, as we approach to our mint we will reveal our second version of our webpage with all the features of web3.
+                                Our developers will design an interactive webpage powered by web3 for our
+                                community. Our first version will be a regular interface, as we approach to
+                                our mint we will reveal our second version of our webpage with all the
+                                features of web3.
                             </div>
                         </div>
                     </Timeline.Item>
@@ -43,7 +46,8 @@ const Roadmap = () => {
                                 Auction sale (99) (Mp4)
                             </div>
                             <div class='mb-2'>
-                                We will be doing a massive auction of 99 unique NFTS which will be live videos of our priceless collectibles ie Meteorites portrayed in 360 degrees.
+                                We will be doing a massive auction of 99 unique NFTS which will be live
+                                videos of our priceless collectibles ie Meteorites portrayed in 360 degrees.
                             </div>
                         </div>
                     </Timeline.Item>
@@ -54,7 +58,9 @@ const Roadmap = () => {
                                 Presale and Public sale mint (3234) (JPEG)
                             </div>
                             <div class='mb-2'>
-                                We conduct presale and public sale of 3234 NFTs which are solely photographs of our precious collectibles. Be ready to get your minds blown by the art, we call 'em Space Arts!
+                                We conduct presale and public sale of 3234 NFTs which are solely
+                                photographs of our precious collectibles. Be ready to get your minds blown
+                                by the art, we call 'em Space Arts!
                             </div>
                         </div>
                     </Timeline.Item>
@@ -65,7 +71,15 @@ const Roadmap = () => {
                                 Meteorite Testing done by a scientist
                             </div>
                             <div class='mb-2'>
-                                As you all can see on our social media channels, we have done the magnetic field test also we could see varieties of metallic elements present in our priceless collectibles. We are pretty much confident just like all other meteorite enthusiasts all over the world that we have nothing other than real ‘Meteorites'. In order to support our statements, we will be doing a scientific test done by a scientist and showcase our reports online on our webpage and social media channels. Be it Meteorite or not, we will keep going forward and realise our wild visions for the future.
+                                As you all can see on our social media channels, we have done the
+                                magnetic field test also we could see varieties of metallic elements
+                                present in our priceless collectibles. We are pretty much confident
+                                just like all other meteorite enthusiasts all over the world that we
+                                have nothing other than real ‘Meteorites'. In order to support our
+                                statements, we will be doing a scientific test done by a scientist and
+                                showcase our reports online on our webpage and social media channels.
+                                Be it Meteorite or not, we will keep going forward and realise our wild
+                                visions for the future.
                             </div>
                         </div>
                     </Timeline.Item>
@@ -85,7 +99,8 @@ const Roadmap = () => {
                                 Limited Liability Company established
                             </div>
                             <div class='mb-2'>
-                                LLC company is established in a crypto friendly country for a better transparency and security of our highly esteemed communit
+                                LLC company is established in a crypto friendly country for a better
+                                transparency and security of our highly esteemed communit
                             </div>
                         </div>
                     </Timeline.Item>
@@ -104,6 +119,51 @@ const Roadmap = () => {
                                 for the launch. We are aiming to build a virtual university fully powered
                                 by our DAO $token where we offer education for literally free of cost for
                                 anyone who hold our $token worth pennies at any market condition.                        </div>
+                        </div>
+                    </Timeline.Item>
+                </Timeline>
+            </div>
+
+            <div>
+                <div className='text-4xl font-semibold 2 pb-8 phase-number'>
+                    Phase 3
+                </div>
+
+                <Timeline className='white-text'>
+                    <Timeline.Item color="#8a8379" className='not-passed'>
+                        <div className='content of timeline'>
+                            <div className='text-2xl font-medium mb-2 mt-[-2px]'>
+                                Lands acquired on the Decentraland
+                            </div>
+                            <div class='mb-2'>
+                                We will acquire multiple lands on the Decetraland and
+                                start building on the metaverse.
+                            </div>
+                        </div>
+                    </Timeline.Item>
+
+                    <Timeline.Item color="#8a8379" className='not-passed'>
+                        <div className='content of timeline'>
+                            <div className='text-2xl font-medium mb-2 mt-[-2px]'>
+                                Realizing our Metaverse vision
+                            </div>
+                            <div class='mb-2'>
+                                Worlds first Meteorite & Precious Stones Market Place is built over Decentraland,
+                                where anyone holding our NFTs would be able to walk in, purchase legit certified
+                                meteorites and precious stones virtually and get them delivered in real life (IRL).
+                                We will build a safe way to collect data from the users, logistic system and will
+                                do our best not to leave any loop holes that threatens the safety and security of
+                                our users. T&C applies. Worlds first virtual university is built over Decentraland,
+                                where anyone holding our $DAO token worth a dollar at any market condition will be
+                                able to walk in and learn world class education by will. There will be wide varieties
+                                of subjects like coding, blockchain, health and wellness, mental health awareness
+                                courses, AI, Space Education etc and much more. This establishment is solely
+                                focussed on bridging the gap between the poor and the rich to a great extent humanly
+                                possible by us as a community. Future is bright indeed!!! We also intend to build
+                                a holder's only conference room where we conduct varieties of events and discussions
+                                in the future. We won't be having a holder's only discord channel for the sake of
+                                safety & security of our investors.
+                            </div>
                         </div>
                     </Timeline.Item>
                 </Timeline>
